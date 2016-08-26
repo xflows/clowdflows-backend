256bcf8ce2cd29323ef51cd5267800f36724606f
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-08-26 14:56:52 +0200

    Adds endpoints for adding special widgets into subprocesses (inputs,outputs,forloops,cv)

--- workflows/api_workflow_viewset.py
+++ workflows/api_workflow_viewset.py
@@ -65,19 +65,262 @@ class WorkflowViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['post'], url_path='subprocess-input')
     def add_subprocess_input(self, request, pk=None):
-        pass
+        workflow = get_object_or_404(Workflow, pk=pk)
+        if workflow.widget == None:
+            message = 'The input widget can only be put in a subprocess.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        else:
+            widget = Widget()
+            widget.workflow = workflow
+            widget.x = 50
+            y = 50
+            while workflow.widgets.filter(y=y, x=widget.x).count() > 0:
+                y = y + 100
+            widget.y = y
+            widget.name = 'Input'
+            widget.type = 'input'
+            widget.save()
+            variable_name = 'Input' + str(widget.pk)
+            output = Output()
+            output.name = 'Input'
+            output.short_name = 'inp'
+            output.variable = variable_name
+            output.widget = widget
+            output.save()
+            input = Input()
+            input.widget = workflow.widget
+            input.name = 'Input'
+            input.short_name = 'inp'
+            input.variable = variable_name
+            input.inner_output = output
+            input.save()
+            output.outer_input = input
+            output.save()
+            widget.defered_outputs = widget.outputs.defer("value").all()
+            widget.defered_inputs = widget.inputs.defer("value").all()
+            widget_data = WidgetSerializer(widget, context={'request': request}).data
+            return HttpResponse(json.dumps(widget_data), 'application/json')
 
     @detail_route(methods=['post'], url_path='subprocess-output')
     def add_subprocess_output(self, request, pk=None):
-        pass
+        workflow = get_object_or_404(Workflow, pk=pk)
+        if workflow.widget == None:
+            message = 'The output widget can only be put in a subprocess.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        else:
+            widget = Widget()
+            widget.workflow = workflow
+            widget.x = 50
+            y = 50
+            while workflow.widgets.filter(y=y, x=widget.x).count() > 0:
+                y = y + 100
+            widget.y = y
+            widget.name = 'Output'
+            widget.type = 'output'
+            widget.save()
+            variable_name = 'Output' + str(widget.pk)
+            input = Input()
+            input.name = 'Output'
+            input.short_name = 'out'
+            input.variable = variable_name
+            input.widget = widget
+            input.save()
+            output = Output()
+            output.widget = workflow.widget
+            output.name = 'Output'
+            output.short_name = 'out'
+            output.variable = variable_name
+            output.inner_input = input
+            output.save()
+            input.outer_output = output
+            input.save()
+            widget.defered_outputs = widget.outputs.defer("value").all()
+            widget.defered_inputs = widget.inputs.defer("value").all()
+            widget_data = WidgetSerializer(widget, context={'request': request}).data
+            return HttpResponse(json.dumps(widget_data), 'application/json')
 
     @detail_route(methods=['post'], url_path='subprocess-forloop')
     def add_subprocess_forloop(self, request, pk=None):
-        pass
+        workflow = get_object_or_404(Workflow, pk=pk)
+        if workflow.widget == None:
+            message = 'The for widgets can only be put in a subprocess.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        elif workflow.widgets.filter(type='for_input').count() > 0:
+            message = 'This subprocess already has a for loop. Try deleting it and adding it again.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        else:
+            for_input = Widget()
+            for_input.workflow = workflow
+            for_input.x = 50
+            y = 50
+            while workflow.widgets.filter(y=y, x=for_input.x).count() > 0:
+                y = y + 100
+            for_input.y = y
+            for_input.name = 'For input'
+            for_input.type = 'for_input'
+            for_input.save()
+            output = Output()
+            output.name = 'For input'  # subproces inner input
+            output.short_name = 'for'
+            output.variable = 'For'
+            output.widget = for_input
+            output.save()
+            input = Input()
+            input.widget = workflow.widget
+            input.name = 'For input'  # subproces input
+            input.short_name = 'for'
+            input.variable = 'For'
+            input.inner_output = output
+            input.save()
+            output.outer_input = input
+            output.save()
+
+            widget = Widget()
+            widget.workflow = workflow
+            widget.x = 200
+            widget.y = 50
+            widget.name = 'For output'
+            widget.type = 'for_output'
+            widget.save()
+            input = Input()
+            input.name = 'For output'
+            input.short_name = 'for'
+            input.variable = 'For'
+            input.widget = widget
+            input.save()
+            output = Output()
+            output.widget = workflow.widget
+            output.name = 'For output'
+            output.short_name = 'for'
+            output.variable = 'For'
+            output.inner_input = input
+            output.save()
+            input.outer_output = output
+            input.save()
+            for_input.defered_outputs = for_input.outputs.defer("value").all()
+            for_input.defered_inputs = for_input.inputs.defer("value").all()
+            widget.defered_outputs = widget.outputs.defer("value").all()
+            widget.defered_inputs = widget.inputs.defer("value").all()
+            widgets = [for_input, widget]
+            widget_data = map(lambda w: WidgetSerializer(w, context={'request': request}).data, widgets)
+            return HttpResponse(json.dumps(widget_data), 'application/json')
 
     @detail_route(methods=['post'], url_path='subprocess-xvalidation')
     def add_subprocess_xvalidation(self, request, pk=None):
-        pass
+        workflow = get_object_or_404(Workflow, pk=pk)
+        if workflow.widget == None:
+            message = 'The cross validation widgets can only be put in a subprocess.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        elif workflow.widgets.filter(type='for_input').count() > 0:
+            message = 'This subprocess already has a for loop. Try deleting it and adding it again.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        elif workflow.widgets.filter(type='cv_input').count() > 0:
+            message = 'This subprocess already has cross validation. Try deleting it and adding it again.'
+            data = json.dumps({'message': message, 'status': 'error'})
+            return HttpResponse(data, 'application/json')
+        else:
+            # input: data
+            cv_input_data = Widget()
+            cv_input_data.workflow = workflow
+            cv_input_data.x = 50
+            y = 50
+            while workflow.widgets.filter(y=y, x=cv_input_data.x).count() > 0:
+                y = y + 100
+            cv_input_data.y = y
+            cv_input_data.name = 'cv input'
+            cv_input_data.type = 'cv_input'
+            cv_input_data.save()
+
+            output = Output()
+            output.name = 'cv input data'
+            output.short_name = 'trn'  # subproces inner input
+            output.variable = 'TRAIN'
+            output.order = 1
+            output.widget = cv_input_data
+            output.save()
+            input = Input()
+            input.widget = workflow.widget
+            input.name = 'cv input data'
+            input.short_name = 'dat'  # subproces input
+            input.variable = 'CVD'
+            input.inner_output = output
+            input.order = 1
+            input.save()
+            output.outer_input = input
+            output.save()
+
+            output = Output()
+            output.name = 'cv input data'
+            output.short_name = 'tst'  # subproces inner input
+            output.variable = 'TEST'
+            output.order = 2
+            output.widget = cv_input_data
+            output.save()
+            input = Input()
+            input.widget = workflow.widget
+            input.name = 'cv input folds'
+            input.short_name = 'cvf'
+            input.variable = 'CVF'
+            input.inner_output = output
+            input.order = 2
+            input.save()
+            output.outer_input = input
+            output.save()
+
+            output = Output()
+            output.name = 'cv input data'
+            output.short_name = 'sed'  # subproces inner input
+            output.variable = 'SEED'
+            output.order = 3
+            output.widget = cv_input_data
+            output.save()
+            input = Input()
+            input.widget = workflow.widget
+            input.name = 'cv input seed'
+            input.short_name = 'sed'
+            input.variable = 'CVS'
+            input.order = 3
+            input.inner_output = output
+            input.save()
+            output.outer_input = input
+            output.save()
+
+            # output
+            widget = Widget()
+            widget.workflow = workflow
+            widget.x = 200
+            widget.y = 50
+            widget.name = 'cv output'
+            widget.type = 'cv_output'
+            widget.save()
+            input = Input()
+            input.name = 'cv output'
+            input.short_name = 'res'
+            input.variable = 'Res'
+            input.widget = widget
+            input.save()
+            output = Output()
+            output.widget = workflow.widget
+            output.name = 'cv output'
+            output.short_name = 'res'
+            output.variable = 'Res'
+            output.inner_input = input
+            output.save()
+            input.outer_output = output
+            input.save()
+            cv_input_data.defered_outputs = cv_input_data.outputs.defer("value").all()
+            cv_input_data.defered_inputs = cv_input_data.inputs.defer("value").all()
+            widget.defered_outputs = widget.outputs.defer("value").all()
+            widget.defered_inputs = widget.inputs.defer("value").all()
+            widgets = [cv_input_data, widget]
+            widget_data = map(lambda w: WidgetSerializer(w, context={'request': request}).data, widgets)
+            return HttpResponse(json.dumps(widget_data), 'application/json')
 
     @detail_route(methods=['post'], url_path='reset')
     def reset(self, request, pk=None):
