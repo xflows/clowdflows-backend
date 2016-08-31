46084837dde00842f90b5879e6e6f4976dc30f86
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-08-31 14:02:56 +0200

    Adds tests for running workflows and all subprocess operations

--- workflows/test_api.py
+++ workflows/test_api.py
@@ -2,11 +2,12 @@ from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from workflows.models import Workflow
+from workflows.models import Workflow, Widget
+from workflows.serializers import WidgetSerializer
 
 TEST_USERNAME = 'testuser'
 TEST_PASSWORD = '123'
-TEST_WORKFLOW_USERS_PK = 3
+TEST_WORKFLOW_USERS_PK = 2
 TEST_WORKFLOW_OTHER_USER_PRIVATE_PK = 4
 TEST_WORKFLOW_OTHER_USER_PUBLIC_PK = 6
 
@@ -26,9 +27,6 @@ class APITests(APITestCase):
             self.assertEqual(response.status_code, code)
 
     def test_widget_library(self):
-        """
-        Ensure we can get the widget library.
-        """
         url = reverse('widget-library-list')
 
         # Test without authentication - this should fail
@@ -139,5 +137,116 @@ class APITests(APITestCase):
             [url_other_user_private, url_other_user_public],
             [status.HTTP_404_NOT_FOUND, status.HTTP_403_FORBIDDEN]
         )
+
+        self._logout()
+
+    def test_run_workflow(self):
+        url = reverse('workflow-run', kwargs={'pk': TEST_WORKFLOW_USERS_PK})
+        url_other_user_private = reverse('workflow-run', kwargs={'pk': TEST_WORKFLOW_OTHER_USER_PRIVATE_PK})
+        url_other_user_public = reverse('workflow-run', kwargs={'pk': TEST_WORKFLOW_OTHER_USER_PUBLIC_PK})
+
+        # Test without authentication - this should not be allowed
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self._login()
+
+        response = self.client.post(url, format="json")
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data['status'], 'ok')
+
+        workflow = Workflow.objects.get(pk=TEST_WORKFLOW_USERS_PK)
+        for widget in workflow.widgets.all():
+            self.assertEqual(widget.finished, True)
+
+        self._test_multiple_response_codes(
+            self.client.post,
+            [url_other_user_private, url_other_user_public],
+            [status.HTTP_404_NOT_FOUND, status.HTTP_403_FORBIDDEN]
+        )
+
+        self._logout()
+
+    def test_subprocess(self):
+        url = reverse('workflow-subprocess', kwargs={'pk': TEST_WORKFLOW_USERS_PK})
+        url_other_user_private = reverse('workflow-subprocess', kwargs={'pk': TEST_WORKFLOW_OTHER_USER_PRIVATE_PK})
+        url_other_user_public = reverse('workflow-subprocess', kwargs={'pk': TEST_WORKFLOW_OTHER_USER_PUBLIC_PK})
+
+        # Test without authentication - this should not be allowed
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self._login()
+
+        response = self.client.post(url, format="json")
+        widget = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(widget['type'], 'subprocess')
+
+        self._test_multiple_response_codes(
+            self.client.post,
+            [url_other_user_private, url_other_user_public],
+            [status.HTTP_404_NOT_FOUND, status.HTTP_403_FORBIDDEN]
+        )
+
+        # Get subprocess workflow object
+        subprocess_workflow = Widget.objects.get(pk=widget['id']).workflow_link
+
+        # Test adding input
+        url = reverse('workflow-subprocess-input', kwargs={'pk': subprocess_workflow.pk})
+        response = self.client.post(url)
+        widget = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(widget['type'], 'input')
+
+        # Test adding output
+        url = reverse('workflow-subprocess-output', kwargs={'pk': subprocess_workflow.pk})
+        response = self.client.post(url)
+        widget = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(widget['type'], 'output')
+
+        self._logout()
+
+    def test_subprocess_forloop(self):
+        url = reverse('workflow-subprocess', kwargs={'pk': TEST_WORKFLOW_USERS_PK})
+
+        self._login()
+
+        # First add a subprocess
+        response = self.client.post(url)
+        widget = response.json()
+        subprocess_workflow = Widget.objects.get(pk=widget['id']).workflow_link
+
+        # Test adding for loop widgets
+        url = reverse('workflow-subprocess-forloop', kwargs={'pk': subprocess_workflow.pk})
+        response = self.client.post(url)
+        data = response.json()
+        self.assertNotIn('status', data)
+        widget_types = {w['type'] for w in data}
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertSetEqual(widget_types, {'for_input', 'for_output'})
+
+        self._logout()
+
+    def test_subprocess_xvalidation(self):
+        url = reverse('workflow-subprocess', kwargs={'pk': TEST_WORKFLOW_USERS_PK})
+
+        self._login()
+
+        # First add a subprocess
+        response = self.client.post(url)
+        data = response.json()
+        self.assertNotIn('status', data)
+        subprocess_workflow = Widget.objects.get(pk=data['id']).workflow_link
+
+        # Test adding cross validation widgets
+        url = reverse('workflow-subprocess-xvalidation', kwargs={'pk': subprocess_workflow.pk})
+        response = self.client.post(url)
+        widgets = response.json()
+        widget_types = {w['type'] for w in widgets}
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertSetEqual(widget_types, {'cv_input', 'cv_output'})
 
         self._logout()
