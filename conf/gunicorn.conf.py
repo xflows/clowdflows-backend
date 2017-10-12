2e6d33719e2b896d6f683d49093ebf55135eb595
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-10-12 13:59:46 +0200

    Updated production config for websockets

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
-bind = "127.0.0.1:8000"
+bind = "unix:/tmp/gunicorn.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 300
 errorlog = '/var/log/cfapi-gunicorn.error.log'
-loglevel = 'debug'
+loglevel = 'info'
