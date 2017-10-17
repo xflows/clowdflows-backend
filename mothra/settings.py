13af642322e86320a03d7a978e786048f741f4e3
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-10-17 15:14:08 +0200

    Fix order of DRF authentication classes

--- mothra/settings.py
+++ mothra/settings.py
@@ -181,9 +181,9 @@ djcelery.setup_loader()
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'PAGINATE_BY': None,
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
