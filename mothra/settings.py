3782d9084876777b212237a71a14b6ec00944bd2
Janez Kranjc <janez.kranjc@ijs.si>
2020-02-17 00:50:25 +0100

    Add setting for redis layers

--- mothra/settings.py
+++ mothra/settings.py
@@ -240,16 +240,25 @@ CELERY_RESULT_BACKEND = 'amqp'
 CELERY_TASK_RESULT_EXPIRES = 18000
 CELERY_TIMEZONE = 'UTC'
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgiref.inmemory.ChannelLayer",
-        # "BACKEND": "asgi_redis.RedisChannelLayer",
-        # "CONFIG": {
-        #    "hosts": [("localhost", 6379)],
-        # },
-        "ROUTING": "mothra.routing.channel_routing",
-    },
-}
+REDIS_LAYER = env.bool("REDIS_LAYER", False)
+
+if REDIS_LAYER:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "asgiref.inmemory.ChannelLayer",
+            "ROUTING": "mothra.routing.channel_routing",
+        },
+    }
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "asgi_redis.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [(env("REDIS_HOST", default="localhost"), env("REDIS_PORT", default=6379))],
+            },
+            "ROUTING": "mothra.routing.channel_routing",
+        },
+    }
 
 DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///mothra.db")}
 
