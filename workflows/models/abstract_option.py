76ababe34501dba0a191c0c02fdfbd637825a4ec
Janez Kranjc <janez.kranjc@ijs.si>
2020-03-03 16:44:23 +0100

    Abstract option uid default to UUID

--- workflows/models/abstract_option.py
+++ workflows/models/abstract_option.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 from workflows.models.abstract_input import AbstractInput
@@ -9,7 +11,7 @@ class AbstractOption(models.Model):
     name = models.CharField(max_length=200)
     value = models.TextField(blank=True)
 
-    uid = models.CharField(max_length=250, blank=True, default='')
+    uid = models.CharField(max_length=250, blank=True, default=uuid.uuid4)
 
     def __str__(self):
         return str(self.name)
